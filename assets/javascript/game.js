53d572fa20a40826c38de20c46070369cd7f81fa
Camilia <mzalicam@gmail.com>
2019-03-28 09:58:18 -0400

    completed game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,67 @@
 // Define all variables
 // Create alphabet array
-var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split(' ');
-var guessesSoFarHTML = document.getElementById('guessesSoFarHTML');
-var guessesLeftHTML = document.getElementById('guessesLeftHTML');
-var winsHTML = document.getElementById('winsHTML');
-var lossesHTML = document.getElementById('lossesHTML');
-// Pick random number
-var computerLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-var guessArray = [];
+var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
+var guessesSoFarHTML = document.getElementById('guessesSoFarHTML');
+
+var guessesLeftHTML = document.getElementById('guessesLeftHTML');
+
+var winsHTML = document.getElementById('winsHTML');
+
+var lossesHTML = document.getElementById('lossesHTML');
+
+// Pick random number
+
+var computerLetter = alphabet[Math.floor(Math.random() *
+  alphabet.length)];
+
+var guessArray = [];
 
 // New Game function
 function newGame(event) {
   guessArray.length = 0;
-  letterHTML.textContent = [];
   guessesSoFarHTML.textContent = [];
   guessesLeftHTML.textContent = "10";
-  computerLetter;
+  computerLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+
 }
-console.log(computerLetter);
 // Computer functions
 document.onkeyup = function (event) {
   // Record user's picked key
   var userGuess = event.key;
-  // Push these into an <array></array>
 
+
+  // Push these into an array
+
+  if (!alphabet.includes(userGuess)) {
+    alert("Pick a letter");
+    return false;
+  }
   guessArray.push(userGuess);
-  guessesSoFarHTML.textContent = `${guessArray}`
+
+  guessesSoFarHTML.textContent = guessArray.join(", ");
+
 
   if (userGuess === computerLetter) {
     var wins = parseInt(winsHTML.textContent) + 1;
     winsHTML.textContent = wins;
     alert("You won!");
+    newGame();
   };
   if (userGuess !== computerLetter) {
     var guessesLeft = parseInt(guessesLeftHTML.textContent) - 1;
     guessesLeftHTML.textContent = guessesLeft;
   }
+
+  if (guessesLeft === 0) {
+    alert("You lose! Try again.");
+    var losses = parseInt(lossesHTML.textContent) + 1;
+    lossesHTML.textContent = losses;
+    newGame();
+  }
 }
+
+newGame();
 
 
 
